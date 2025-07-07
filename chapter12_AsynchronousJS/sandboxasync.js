b7196d4dd35db133004f91f637c5ee5195388385
espacio09 <166817287+espacio09@users.noreply.github.com>
2025-07-07 13:57:14 +0000

    95 Chaining promises

--- chapter12_AsynchronousJS/sandboxasync.js
+++ chapter12_AsynchronousJS/sandboxasync.js
@@ -248,3 +248,48 @@ getTodos('todos/luigi.json').then(data => {
 */
 
 
+/////////////////////////////////////////////
+//    95. Chaining Promises
+///       use promises to manage network requests
+
+const getTodos = (resource) => {
+
+    return new Promise((resolve, reject)  => {
+    const request = new XMLHttpRequest();
+
+request.addEventListener('readystatechange'),() => {
+if(request.readyState === 4 && request.status === 200){
+    const data = JSON.parse(request.responseText);
+    resolve(data);
+    };
+
+} else if(request.readyState === 4){
+    reject('error getting resource');
+}
+});
+
+request.open('GET, resource);
+request.send();
+
+});
+};
+
+
+// .then takes in a function - catch for the error for all of the functions
+//  promise fires for every function
+
+getTodos('todos/luigi.json').then(data => {
+    console.log('promise 1 resolved:', data);
+    return getTodos('todos/mario.json');
+}).then(data =>  {
+    console.log('promise 2 resolved:', data);
+    return getTodos('todos/shaun.json');
+ }).then(data =>  {
+    console.log('promise 3 resolved:', data);
+}).catch(err => {
+    console.log('promise rejected:' , err);
+});
+
+
+
+
