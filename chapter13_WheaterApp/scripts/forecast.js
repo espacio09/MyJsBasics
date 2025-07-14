86d8e0a25e3a0d3886f7245d84f112b0a6681901
espacio09 <166817287+espacio09@users.noreply.github.com>
2025-07-14 13:43:27 +0000

    get city info via api key

---
+++ chapter13_WheaterApp/scripts/forecast.js
@@ -0,0 +1,36 @@
+//   here you have to insert: api key from accu weather developper website
+//    https://developer.accuweather.com/  
+
+// in accuweather have the api key for city search (get city search) - with this get then the
+//  temperature
+
+
+const key = 'place api key here from accuweather - which is a random string '
+
+// get the city from api key on accuweather - to make the request to the api end point with URL
+// below
+
+const getCity = async () => {
+
+        const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+// add query parameters to the end of an URL in an template string
+        const query =`?apikey=${key}&q=${city}`;
+
+//fetch the resource base and query concatonated by
+        const response = await fetch(base + query);
+
+// get Json method on the response: - resolved data is in data constant
+        const data = await response.json();
+    
+        // closest match in the example for manchester is 0 in the array
+       //  console.log(data[0]);
+
+       return data[0];
+};
+
+// call the method getCity
+
+getCity('manchester')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
