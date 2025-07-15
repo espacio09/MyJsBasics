104cf78300e5f8be59da9784be958e02f17ebeeb
espacio09 <166817287+espacio09@users.noreply.github.com>
2025-07-15 08:26:11 +0000

    updating location

--- chapter13_WheaterApp/scripts/app.js
+++ chapter13_WheaterApp/scripts/app.js
@@ -4,8 +4,21 @@
 const cityForm = document.querySelector('form');
 
 //update city when entering new city - with async function with weather id and city info
-const updateCity = async () => {
+//this async function returns a promise
+
+const updateCity = async (city) => {
     
+    //console.log(city);    //for testing purpose that submit of city works
+    
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+
+    // return the properties: and data in an object
+    return {
+
+        cityDets: cityDets,
+        weather: weather,
+    }
 }
 
 cityForm.addEventListener('submit', e =>{
@@ -17,9 +30,9 @@ cityForm.addEventListener('submit', e =>{
     const city = cityForm.city.value.trin();
     cityForm.reset();   // clearout the form fields with reset
 
-    // update the ui with new city
+    // update the ui with new city - is also an async function
 
-
-
-
+    updateCity(city)
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
 } )
