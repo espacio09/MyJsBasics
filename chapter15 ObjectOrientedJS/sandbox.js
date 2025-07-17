45c690a49a83151ec0a2f60383fe403e17b71a9b
espacio09 <166817287+espacio09@users.noreply.github.com>
2025-07-17 14:07:04 +0000

    method chaining

--- chapter15 ObjectOrientedJS/sandbox.js
+++ chapter15 ObjectOrientedJS/sandbox.js
@@ -33,11 +33,53 @@ userTwo.login();
 
 //////    this all would result in a mess creating x-users so there is a better
 //////    way to do this
+/////     Classes in JS
 
-const userThree = new User();
+//       built a class with inside a constructor function
+//       give properties to the instance in the constructor
+
+class User{
+    constructor(){
+        // set up properties on the object
+        this.username = username;
+        this.email = email;
+        this.score = 0;
+
+    }
+    //  here goes the method - not in the constructor
+    //  give methods to users
+
+    login(){
+        console.log(`${this.username} just logged in`);
+        return this;
+    }
+    logout(){
+        console.log(`${this.username}just logged out`);
+        return this;
+
+    }
+    incScore(){
+        this.score += 1;
+        console.log(`${this.username}has a score of ${this.score}`);
+        return this;
+
+    }
+}
+
+const userOne = new User('mario', 'mario@thenetninja.co.uk');
+const userTwo = new User('luigi', 'luigi@thenetninja.co.uk');
+
+
+console.log(userOne, userTwo);
+// method chaining
+userOne.login().incScore().incScore().logout();
 
 
 
+// the 'new' keyword
+// 1 - it creates a new emtpy object {}
+// 2 - it binds the value of 'this' to the new empty object
+// 3 - it calls the constructor function to 'build' the object
 
 
 
