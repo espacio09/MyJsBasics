1e8da9adb567aa3ec2c62d958ef4c18f8979ebaa
Tatjana Klatyik <TatjanaKlatyik@aol.com>
2025-08-11 12:34:54 +0200

    added favicon

--- chapter16_DB_Firebase/sandbox.js
+++ chapter16_DB_Firebase/sandbox.js
@@ -1,19 +1,27 @@
+const list = document.querySelector('ul');
+
+const addRecipe = (recipe) => {
+  let html = `
+  <li>
+  
+    <div class="d-flex justify-content-between align-items-center">
+      <span>${recipe.title}</span> 
+      </div>
+    </li>
+  `;
+  list.innerHTML += html;
+}
 
 
-   // Add a new recipe
-    const form = document.querySelector('form');
-    form.
-
-addEventListener('submit', (e) => {
-    e.preventDefault();
-    const recipe = e.target.recipe.value;
-    console.log(recipe);
-    // Add the recipe to the database
-    // db.collection('recipes').add({ title: recipe })
-    //   .then(() => {
-    //     console.log('Recipe added');
-    //   })
-    //   .catch((error) => {
-    //     console.error('Error adding recipe: ', error);
-    //   });
+ //   Add the recipe to the database
+  
+addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const recipe = e.target.recipe.value;
+  try {
+    await addDoc(recipesCol, { title: recipe });
+    console.log('Recipe added');
+  } catch (error) {
+    console.error('Error adding recipe: ', error);
+  }
   });   
